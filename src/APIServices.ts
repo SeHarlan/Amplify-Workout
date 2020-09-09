ed5b1a1394ecd443de855adf189e349dbf51e0e6
Scott <seharlan@gmail.com>
2020-09-08 19:22:44 -0700

    transfered to typescript

---
+++ src/APIServices.ts
@@ -0,0 +1,117 @@
+import { API, graphqlOperation } from 'aws-amplify'
+import { createWorkout, updateWorkout, deleteWorkout } from './graphql/mutations'
+import { listWorkouts } from './graphql/queries'
+
+interface workoutInterface {
+  id: string,
+  name: string,
+  description: string,
+  light: number,
+  medium: number,
+  heavy: number
+}
+
+interface dataInterface {
+  id: string,
+  name: string,
+  description: string,
+  light: number,
+  medium: number,
+  heavy: number,
+  createdAt: Date,
+  updatedAt: Date
+}
+
+interface formStateInterface {
+  name: string,
+  description: string,
+}
+
+export const initialState = {
+  name: '',
+  description: '',
+}
+
+export async function fetchWorkouts(setWorkouts: Function) {
+  try {
+    const workoutData: any = await API.graphql(graphqlOperation(listWorkouts))
+    const workouts: dataInterface[] = workoutData.data.listWorkouts.items
+    const mungedWorkouts = workouts.map(({
+      id,
+      name,
+      description,
+      light,
+      medium,
+      heavy }) => {
+      return {
+        id,
+        name,
+        description,
+        light,
+        medium,
+        heavy
+      }
+    })
+    setWorkouts(mungedWorkouts)
+  } catch (err) { console.log('error fetching Workouts: ', err) }
+}
+
+export async function addWorkout(formState: formStateInterface, setFormState: Function, setWorkouts: Function, workouts: workoutInterface[]) {
+  try {
+    if (!formState.name || !formState.description) return
+    const workout = {
+      ...formState,
+      light: 0,
+      medium: 0,
+      heavy: 0
+    }
+    setFormState(initialState)
+
+    const { data }: any = await API.graphql(graphqlOperation(createWorkout, { input: workout }))
+
+    const {
+      id,
+      name,
+      description,
+      light,
+      medium,
+      heavy
+    }: workoutInterface = data.createWorkout
+
+    const returnedWorkout = {
+      id,
+      name,
+      description,
+      light,
+      medium,
+      heavy
+    }
+
+    setWorkouts([...workouts, returnedWorkout])
+  } catch (err) {
+    console.log('error creating workout: ', err)
+  }
+}
+
+export async function removeWorkout(index: number, workouts: workoutInterface[], setWorkouts: Function) {
+  try {
+    const copyWorkouts = [...workouts]
+    const [deletedWorkout] = copyWorkouts.splice(index, 1)
+    setWorkouts(copyWorkouts)
+
+    await API.graphql(graphqlOperation(deleteWorkout, { input: { id: deletedWorkout.id } }))
+  } catch (err) {
+    console.log('error deleting workout: ', err)
+  }
+}
+
+
+export async function setWeight(index: number, key: string, weight: number, workouts: workoutInterface[], setWorkouts: Function) {
+  try {
+    const copyWorkouts: any = [...workouts]
+    copyWorkouts[index][key] = weight
+    setWorkouts(copyWorkouts)
+
+    await API.graphql(graphqlOperation(updateWorkout, { input: copyWorkouts[index] }))
+  } catch (err) { console.log('error updating weight: ', err) }
+}
--- src/App.js
+++ src/App.js
@@ -1,97 +1,114 @@
 /* src/App.js */
 import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
-import { createWorkout, updateWorkout, deleteWorkout } from './graphql/mutations.ts'
-import { listWorkouts } from './graphql/queries.ts'
+import Amplify from 'aws-amplify'
+// import Amplify, { API, graphqlOperation } from 'aws-amplify'
+// import { createWorkout, updateWorkout, deleteWorkout } from './graphql/mutations.ts'
+// import { listWorkouts } from './graphql/queries.ts'
+import { fetchWorkouts, addWorkout, setWeight, removeWorkout, initialState } from './APIServices.ts'
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
-
-const initialState = {
-  name: '',
-  description: '',
-}
 
 const App = () => {
   const [formState, setFormState] = useState(initialState)
   const [workouts, setWorkouts] = useState([])
 
   useEffect(() => {
-    fetchWorkouts()
+    fetchWorkouts(setWorkouts)
   }, [])
 
   function setInput(key, value) {
     setFormState({ ...formState, [key]: value })
   }
 
-  async function fetchWorkouts() {
-    try {
-      const workoutData = await API.graphql(graphqlOperation(listWorkouts))
-      const workouts = workoutData.data.listWorkouts.items
-      const mungedWorkouts = workouts.map(({
-        id,
-        name,
-        description,
-        light,
-        medium,
-        heavy }) => {
-        return {
-          id,
-          name,
-          description,
-          light,
-          medium,
-          heavy
-        }
-      })
-      setWorkouts(mungedWorkouts)
-    } catch (err) { console.log('error fetching Workouts: ', err) }
-  }
+  // async function fetchWorkouts() {
+  //   try {
+  //     const workoutData = await API.graphql(graphqlOperation(listWorkouts))
+  //     const workouts = workoutData.data.listWorkouts.items
+  //     const mungedWorkouts = workouts.map(({
+  //       id,
+  //       name,
+  //       description,
+  //       light,
+  //       medium,
+  //       heavy }) => {
+  //       return {
+  //         id,
+  //         name,
+  //         description,
+  //         light,
+  //         medium,
+  //         heavy
+  //       }
+  //     })
+  //     setWorkouts(mungedWorkouts)
+  //   } catch (err) { console.log('error fetching Workouts: ', err) }
+  // }
 
-  async function addWorkout() {
-    try {
-      if (!formState.name || !formState.description) return
-      const workout = {
-        ...formState,
-        light: 0,
-        medium: 0,
-        heavy: 0
-      }
-      setWorkouts([...workouts, workout])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createWorkout, { input: workout }))
-    } catch (err) {
-      console.log('error creating workout: ', err)
-    }
-  }
+  // async function addWorkout() {
+  //   try {
+  //     if (!formState.name || !formState.description) return
+  //     const workout = {
+  //       ...formState,
+  //       light: 0,
+  //       medium: 0,
+  //       heavy: 0
+  //     }
+  //     setFormState(initialState)
 
-  async function removeWorkout(index) {
-    try {
-      const copyWorkouts = [...workouts]
-      const [deletedWorkout] = copyWorkouts.splice(index, 1)
-      setWorkouts(copyWorkouts)
+  //     const { data } = await API.graphql(graphqlOperation(createWorkout, { input: workout }))
 
-      await API.graphql(graphqlOperation(deleteWorkout, { input: { id: deletedWorkout.id } }))
-    } catch (err) {
-      console.log('error deleting workout: ', err)
-    }
-  }
+  //     const {
+  //       id,
+  //       name,
+  //       description,
+  //       light,
+  //       medium,
+  //       heavy
+  //     } = data.createWorkout
+
+  //     const returnedWorkout = {
+  //       id,
+  //       name,
+  //       description,
+  //       light,
+  //       medium,
+  //       heavy
+  //     }
+
+  //     setWorkouts([...workouts, returnedWorkout])
+  //   } catch (err) {
+  //     console.log('error creating workout: ', err)
+  //   }
+  // }
+
+  // async function removeWorkout(index) {
+  //   try {
+  //     const copyWorkouts = [...workouts]
+  //     const [deletedWorkout] = copyWorkouts.splice(index, 1)
+  //     setWorkouts(copyWorkouts)
+
+  //     await API.graphql(graphqlOperation(deleteWorkout, { input: { id: deletedWorkout.id } }))
+  //   } catch (err) {
+  //     console.log('error deleting workout: ', err)
+  //   }
+  // }
 
 
-  async function setWeight(index, key, weight) {
-    try {
-      const copyWorkouts = [...workouts]
-      copyWorkouts[index][key] = weight
-      setWorkouts(copyWorkouts)
+  // async function setWeight(index, key, weight) {
+  //   try {
+  //     const copyWorkouts = [...workouts]
+  //     copyWorkouts[index][key] = weight
+  //     setWorkouts(copyWorkouts)
 
-      await API.graphql(graphqlOperation(updateWorkout, { input: copyWorkouts[index] }))
-    } catch (err) { console.log('error updating weight: ', err) }
+  //     await API.graphql(graphqlOperation(updateWorkout, { input: copyWorkouts[index] }))
+  //   } catch (err) { console.log('error updating weight: ', err) }
 
-  }
+  // }
 
   return (
     <div style={styles.container}>
-      <h2>Amplify workouts</h2>
+      <h2>My Workouts</h2>
       <input
         onChange={event => setInput('name', event.target.value)}
         style={styles.input}
@@ -104,7 +121,7 @@ const App = () => {
         value={formState.description}
         placeholder="Description"
       />
-      <button style={styles.button} onClick={addWorkout}>Create Workout</button>
+      <button style={styles.button} onClick={() => addWorkout(formState, setFormState, setWorkouts, workouts)}>Create Workout</button>
       {
         workouts.map((workout, index) => (
           <div key={workout.id ? workout.id : index} style={styles.workout}>
@@ -117,7 +134,7 @@ const App = () => {
                 type="number"
                 min="0"
                 onChange={({ target }) => {
-                  setWeight(index, "light", target.value)
+                  setWeight(index, "light", target.value, workouts, setWorkouts)
                 }}
                 style={styles.input}
                 value={workouts[index].light || 0}
@@ -130,7 +147,7 @@ const App = () => {
                 type="number"
                 min="0"
                 onChange={({ target }) => {
-                  setWeight(index, "medium", target.value)
+                  setWeight(index, "medium", target.value, workouts, setWorkouts)
                 }}
                 style={styles.input}
                 value={workouts[index].medium || 0}
@@ -143,13 +160,13 @@ const App = () => {
                 type="number"
                 min="0"
                 onChange={({ target }) => {
-                  setWeight(index, "heavy", target.value)
+                  setWeight(index, "heavy", target.value, workouts, setWorkouts)
                 }}
                 style={styles.input}
                 value={workouts[index].heavy || 0}
               />
             </div>
-            <button style={styles.button} onClick={() => removeWorkout(index)}>Delete</button>
+            <button style={styles.button} onClick={() => removeWorkout(index, workouts, setWorkouts)}>Delete</button>
           </div>
         ))
       }
